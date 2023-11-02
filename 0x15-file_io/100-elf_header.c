8201e8e5fb33bba55a98739cf8d4b28c04b0d756
jimmy <jimmymwas@gmail.com>
2023-11-02 03:16:57 -0700

    0x15. C - File I/O

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,22 @@
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Function prototypes */
+/* Other function prototypes remain unchanged */
+/**
+* main - Displays information in the ELF header at the start of an ELF file.
+* @argc: Number of arguments supplied to the program.
+* @argv: An array of pointers to the arguments.
+*
+* Return: Always returns 0.
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+/* Your main function's code remains unchanged */
+return (0); /* Return statement with parentheses */
+}
